69fba749ecdb3052f23b7cc480219071b349b6c4
Vikctar <vikc.steve@gmail.com>
2017-06-19 16:00:23 +0300

    NullPointer

---
+++ app/src/main/java/com/vikcandroid/gmaillike/adapter/MessagesAdapter.java
@@ -0,0 +1,281 @@
+package com.vikcandroid.gmaillike.adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.SparseBooleanArray;
+import android.view.HapticFeedbackConstants;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.vikcandroid.gmaillike.R;
+import com.vikcandroid.gmaillike.helper.CircleTransform;
+import com.vikcandroid.gmaillike.helper.FlipAnimator;
+import com.vikcandroid.gmaillike.model.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by android-dev on 6/19/17.
+ */
+
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessagesViewHolder> {
+
+    private Context mContext;
+    private List<Message> messages;
+    private MessageAdapterListener listener;
+    private SparseBooleanArray selectedItems;
+
+    // array used to perform multiple animation at once
+    private SparseBooleanArray animationItemsIndex;
+    private boolean reverseAllAnimations = false;
+
+    // index is used to animate only the selected row
+    // dirty fix, find a better solution
+    private static int currentSelectedIndex = -1;
+
+    public MessagesAdapter(Context mContext, List<Message> messages, MessageAdapterListener listener) {
+        this.mContext = mContext;
+        this.messages = messages;
+        this.listener = listener;
+        selectedItems = new SparseBooleanArray();
+        animationItemsIndex = new SparseBooleanArray();
+    }
+
+    @Override
+    public MessagesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_list_row, parent, false);
+
+        return new MessagesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MessagesViewHolder holder, int position) {
+        Message message = messages.get(position);
+
+        // displaying text view data
+        holder.from.setText(message.getFrom());
+        holder.subject.setText(message.getSubject());
+        holder.message.setText(message.getMessage());
+        holder.timestamp.setText(message.getTimestamp());
+
+        // displaying the first letter of From in icon text
+        holder.iconText.setText(message.getFrom().substring(0, 1));
+
+        // change the row state to activated
+        holder.itemView.setActivated(selectedItems.get(position, false));
+
+        // change the font style depending on message read status
+        applyReadStatus(holder, message);
+
+        // handle message star
+        applyImportant(holder, message);
+
+        // handle icon animation
+        applyIconAnimation(holder, position);
+
+        // display profile image
+        applyProfilePicture(holder, message);
+
+        // apply click events
+        applyClickEvents(holder, position);
+    }
+
+    private void applyClickEvents(MessagesViewHolder holder, final int position) {
+        holder.iconContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onIconClicked(position);
+            }
+        });
+
+        holder.iconImp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onIconImportantClicked(position);
+            }
+        });
+
+        holder.messageContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onMessageRowClicked(position);
+            }
+        });
+
+        holder.messageContainer.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                listener.onRowLongClicked(position);
+                v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+                return true;
+            }
+        });
+    }
+
+    private void applyProfilePicture(MessagesViewHolder holder, Message message) {
+        if (!TextUtils.isEmpty(message.getPicture())) {
+            Glide.with(mContext).load(message.getPicture())
+                    .thumbnail(0.5f)
+                    .crossFade()
+                    .transform(new CircleTransform(mContext))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(holder.imgProfile);
+            holder.imgProfile.setColorFilter(null);
+            holder.iconText.setVisibility(View.GONE);
+        } else {
+            holder.imgProfile.setImageResource(R.drawable.bg_circle);
+            holder.imgProfile.setColorFilter(message.getColor());
+            holder.iconText.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void applyIconAnimation(MessagesViewHolder holder, int position) {
+        if (selectedItems.get(position, false)) {
+            holder.iconFront.setVisibility(View.GONE);
+            resetIconYAxis(holder.iconBack);
+            holder.iconBack.setVisibility(View.VISIBLE);
+            holder.iconBack.setAlpha(1);
+            if (currentSelectedIndex == position) {
+                FlipAnimator.flipView(mContext, holder.iconBack, holder.iconFront, true);
+                resetCurrentIndex();
+            }
+        } else {
+            holder.iconBack.setVisibility(View.GONE);
+            resetIconYAxis(holder.iconFront);
+            holder.iconFront.setVisibility(View.VISIBLE);
+            holder.iconFront.setAlpha(1);
+            if ((reverseAllAnimations && animationItemsIndex.get(position, false)) || currentSelectedIndex == position) {
+                FlipAnimator.flipView(mContext, holder.iconBack, holder.iconFront, false);
+                resetCurrentIndex();
+            }
+        }
+    }
+
+    // As the views will be reused, sometimes the icon appears as
+    // flipped because older view is reused. Reset the Y-axis to 0
+    private void resetIconYAxis(View view) {
+        if (view.getRotationY() != 0) {
+            view.setRotationY(0);
+        }
+    }
+
+    public void resetAnimationIndex() {
+        reverseAllAnimations = false;
+        animationItemsIndex.clear();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return messages.get(position).getId();
+    }
+
+    private void applyImportant(MessagesViewHolder holder, Message message) {
+        if (message.isImportant()) {
+            holder.iconImp.setImageDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_star_black_24dp));
+            holder.iconImp.setColorFilter(ContextCompat.getColor(mContext, R.color.icon_tint_selected));
+        } else {
+            holder.iconImp.setImageDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_star_border_black_24dp));
+            holder.iconImp.setColorFilter(ContextCompat.getColor(mContext, R.color.icon_tint_normal));
+        }
+    }
+
+    private void applyReadStatus(MessagesViewHolder holder, Message message) {
+        if (message.isRead()) {
+            holder.from.setTypeface(null, Typeface.NORMAL);
+            holder.subject.setTypeface(null, Typeface.NORMAL);
+            holder.from.setTextColor(ContextCompat.getColor(mContext, R.color.subject));
+            holder.subject.setTextColor(ContextCompat.getColor(mContext, R.color.message));
+        } else {
+            holder.from.setTypeface(null, Typeface.BOLD);
+            holder.subject.setTypeface(null, Typeface.BOLD);
+            holder.from.setTextColor(ContextCompat.getColor(mContext, R.color.from));
+            holder.subject.setTextColor(ContextCompat.getColor(mContext, R.color.subject));
+        }
+    }
+
+    public void toggleSelection(int pos) {
+        currentSelectedIndex = pos;
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+            animationItemsIndex.delete(pos);
+        } else {
+            selectedItems.put(pos, true);
+            animationItemsIndex.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        reverseAllAnimations = true;
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems() {
+        List<Integer> items =
+                new ArrayList<>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
+
+    public void removeData(int position) {
+        messages.remove(position);
+        resetCurrentIndex();
+    }
+
+    private void resetCurrentIndex() {
+        currentSelectedIndex = -1;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public class MessagesViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+        public TextView from, subject, message, iconText, timestamp;
+        public ImageView iconImp, imgProfile;
+        public LinearLayout messageContainer;
+        public RelativeLayout iconContainer, iconBack, iconFront;
+
+
+        public MessagesViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            listener.onRowLongClicked(getAdapterPosition());
+            v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+            return true;
+        }
+    }
+
+    public interface MessageAdapterListener {
+        void onIconClicked(int position);
+
+        void onIconImportantClicked(int position);
+
+        void onMessageRowClicked(int position);
+
+        void onRowLongClicked(int position);
+    }
+}
