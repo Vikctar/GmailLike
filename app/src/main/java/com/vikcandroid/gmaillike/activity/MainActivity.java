84f1ebb27847e2e4c97369610f05288f994d2165
Vikctar <vikc.steve@gmail.com>
2017-06-19 11:50:00 +0300

    Initial commit

---
+++ app/src/main/java/com/vikcandroid/gmaillike/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vikcandroid.gmaillike.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vikcandroid.gmaillike.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
